3e4c68bb0ab03c5df3658186b56fd3b1afcba307
rynao <94769118+rynao@users.noreply.github.com>
2022-01-29 11:22:51 +0900

    最低限のバリデーション設定

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,8 @@
 class Contract < ApplicationRecord
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :rent, presence: true, numericality: true
+
   belongs_to :property
   belongs_to :room
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,10 @@
 class Property < ApplicationRecord
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :building, presence: true
+  validates :business_entity, presence: true
+  validates :total_units, numericality: true
+
   has_many :rooms, dependent: :destroy
   has_many :contracts, dependent: :destroy
 end
